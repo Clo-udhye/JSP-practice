15e43ce1937f5397ff73dc85ff166ecce66ed629
ParkDahye <clo_udhye@naver.com>
2021-01-07 09:18:25 +0900

    [Add] 2021-01-06 Board Simple - MVC Model2 - Controller

---
+++ Model2Ex01/src/model2/ModifyOkAction.java
@@ -0,0 +1,32 @@
+package model2;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model1.BoardDAO;
+import model1.BoardTO;
+
+public class ModifyOkAction implements BoardAction {
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) {
+		// TODO Auto-generated method stub
+		System.out.println("ModifyOkAction");
+		
+		BoardTO to = new BoardTO();
+		to.setSeq(request.getParameter("seq"));
+		to.setPassword(request.getParameter("password"));		
+		to.setSubject(request.getParameter("subject"));
+		to.setContent(request.getParameter("content"));
+		to.setMail("");
+		if(!request.getParameter("mail1").equals("") && !request.getParameter("mail2").equals("")){
+			to.setMail(request.getParameter("mail1") + "@" + request.getParameter("mail2"));
+		}
+
+		BoardDAO dao = new BoardDAO();
+		int flag = dao.boardModifyOk(to);
+		
+		request.setAttribute("flag", flag);
+	}
+
+}
